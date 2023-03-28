bf6410c3480dc7d5b456000cffb5db020c5025ff
xavier <xavierassad@hotmail.com>
2023-03-28 02:32:08 -0700

    Commented author in Forms

--- forms.py
+++ forms.py
@@ -12,7 +12,7 @@ ckeditor = CKEditor(app)
 class CreatePostForm(FlaskForm):
     title = StringField("Blog Post Title", validators=[DataRequired()])
     subtitle = StringField("Subtitle", validators=[DataRequired()])
-    author = StringField("Your Name", validators=[DataRequired()])
+    # author = StringField("Your Name", validators=[DataRequired()])
     img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
     body = CKEditorField("Blog Content", validators=[DataRequired()])
     submit = SubmitField("Submit Post")
